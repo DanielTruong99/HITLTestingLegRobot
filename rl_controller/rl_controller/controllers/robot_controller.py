bb8e896ce8421a9566f8a01b0b740c6151124564
DanielTruong999 <datthanh01237344449@gmail.com>
2025-02-22 13:55:20 +0900

    Change structure code for FSM

--- rl_controller/rl_controller/controllers/robot_controller.py
+++ rl_controller/rl_controller/controllers/robot_controller.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 
-from ..controllers import state_machine
+from .state_machine import RobotFSM, RobotEvent
 from .robot import Robot
 from .robot_config import RobotConfig
 from .policy_controller import PolicyController
@@ -34,15 +34,36 @@ class RobotController(object):
         # Initialize the command, action
         self._command = np.zeros(3)
 
+        # Initialize some special messages 
+        self._joint_cmd_stop_msg = JointState()
+        self._joint_cmd_stop_msg.position = [0.0] * self.robot.num_joints
+        kps = [0.0] * self.robot.num_joints
+        kds = [0.0] * self.robot.num_joints
+        self._joint_cmd_stop_msg.velocity = kps + kds
+        
+        self._joint_cmd_damping_msg = JointState()
+        self._joint_cmd_damping_msg.position = [0.0] * self.robot.num_joints
+        kps = [1000.0] * self.robot.num_joints
+        kds = [10.0] * self.robot.num_joints
+        self._joint_cmd_damping_msg.velocity = kps + kds
+
+    def stop(self) -> None:
+        """
+        Stop the robot controller.
+        """
+        self.node_handler.joint_cmd_pub.publish(self._joint_cmd_stop_msg)
+
     def run(self) -> None:
         """
         Run the robot controller.
         """
         if len(self.event_queue) > 0:
             event = self.event_queue.pop(0)
-            self.state_machine.dispatch(event)
+        else:
+            event = RobotEvent.DEFAULT_SIG    
+        self.state_machine.dispatch(event)
 
-    def push_event(self, event: state_machine.BuiltInEvent) -> None:
+    def push_event(self, event: RobotEvent) -> None:
         """
         Push the event to the event queue.
 
@@ -81,127 +102,3 @@ class RobotController(object):
             np.ndarray: the joint cmds
         """
         return self.current_controller.forward(self._command)
-
-@dataclass
-class RobotEvent(state_machine.BuiltInEvent):
-    TIME_OUT_2S = state_machine.BuiltInEvent.USER_SIG
-    TIMER_EVENT = state_machine.BuiltInEvent.USER_SIG + 1
- 
-
-class RobotFSM(state_machine.FSM):
-    def __init__(self, robot: Robot, controller: RobotController) -> None:
-        """
-        Initialize the robot FSM.
-
-        Args:
-            config (RobotConfig): the configuration object
-        """
-        super().__init__()
-        self.robot: Robot = robot
-        self.controller = controller
-
-    def initial_state(self, event: RobotEvent) -> state_machine.Status:
-        """
-        The initial state of the robot FSM.
-        
-        Args:
-            event (RobotEvent): the event
-
-        Returns:
-            state_machine.Status: the status
-        """
-        status = state_machine.Status.IGNORED_STATUS
-
-        if event is RobotEvent.ENTRY_SIG:
-            rclpy.logging._root_logger.info("Robot in initial state")
-            status = state_machine.Status.TRAN_STATUS
-
-        else:
-            if self.robot.is_ready:
-                rclpy.logging._root_logger.info("All sensor datas are ready")
-                self.transition_to(self.configuration_state)
-                status = state_machine.Status.TRAN_STATUS
-
-        return status
-    
-    def configuration_state(self, event: RobotEvent) -> state_machine.Status:
-        """
-        The configuration state of the robot FSM.
-        
-        Args:
-            event (RobotEvent): the event
-                
-        Returns:
-            state_machine.Status: the status
-        """
-        status = state_machine.Status.IGNORED_STATUS
-
-        if event is RobotEvent.ENTRY_SIG:
-            rclpy.logging._root_logger.info("Robot in configuration state")
-            # set pd gain 
-            # Move to default joint
-
-            # Set the timer_counter to 0
-            self.controller.node_handler.timer_counter = 0
-            status = state_machine.Status.HANDLED_STATUS
-
-        elif event is RobotEvent.TIME_OUT_2S:
-            self.transition_to(self.running_state)
-            status = state_machine.Status.TRAN_STATUS
-
-
-        return status
-    
-    def running_state(self, event: RobotEvent) -> state_machine.Status:
-        """
-        The running state of the robot FSM.
-        
-        Args:
-            event (RobotEvent): the event
-                
-        Returns:
-            state_machine.Status: the status
-        """
-        status = state_machine.Status.IGNORED_STATUS
-
-        # 50Hz
-        if event is RobotEvent.TIMER_EVENT:
-            # Compute the joint commands
-            joint_cmds = self.controller.compute().tolist()
-            
-            # Publish the joint commands
-            joint_state = JointState()
-            for joint_cmd in joint_cmds:
-                joint_state.position.append(joint_cmd)
-            self.node_handler.joint_cmd_pub.publish(joint_state)
-
-            status = state_machine.Status.HANDLED_STATUS
-        
-        # Default
-        else:
-            # check safety
-            status = state_machine.Status.HANDLED_STATUS
-
-
-        return status
-
-    def error_state(self, event: RobotEvent) -> state_machine.Status:
-        """
-        The error state of the robot FSM.
-        
-        Args:
-            event (RobotEvent): the event
-                
-        Returns:
-            state_machine.Status: the status
-        """
-        status = state_machine.Status.IGNORED_STATUS
-
-        if event is RobotEvent.ENTRY_SIG:
-            # stop the robot
-            # log error
-            status = state_machine.Status.HANDLED_STATUS
-
-        return status
-
-
--- rl_controller/rl_controller/controllers/state_machine.py
+++ rl_controller/rl_controller/controllers/state_machine.py
@@ -1,60 +1,135 @@
-from enum import Enum
+import rclpy
+from sensor_msgs.msg import JointState
 from dataclasses import dataclass
+from .robot_controller import RobotController
+from .robot import Robot
+from ..finite_state_machine import fsm as state_machine
 
 @dataclass
-class BuiltInEvent:
-    INIT_SIG = 1
-    ENTRY_SIG = 2
-    EXIT_SIG = 3
-    USER_SIG = 4
+class RobotEvent(state_machine.BuiltInEvent):
+    TIME_OUT_2S = state_machine.BuiltInEvent.USER_SIG
+    TIMER_EVENT = state_machine.BuiltInEvent.USER_SIG + 1
+ 
 
-@dataclass
-class Status:
-    TRAN_STATUS = 1
-    HANDLED_STATUS = 2
-    IGNORED_STATUS = 3
-    INIT_STATUS = 4
+class RobotFSM(state_machine.FSM):
+    def __init__(self, robot: Robot, controller: RobotController) -> None:
+        """
+        Initialize the robot FSM.
 
+        Args:
+            config (RobotConfig): the configuration object
+        """
+        super().__init__()
+        self.robot: Robot = robot
+        self.controller = controller
 
-"""
-    while true:
-        if event_queue
-            event = event_queue.pop(0)
-            state_machine.dispatch(event)
+    def initial_state(self, event: RobotEvent) -> state_machine.Status:
+        """
+        The initial state of the robot FSM.
         
-    
-    def dispatch(self, event):
-        prev_state = self.state
-        status = self.state(event)
+        Args:
+            event (RobotEvent): the event
 
-        if status is State.TRAN_STATUS:
-            prev_state(self, BuiltInEvent.EXIT_SIG)
-            self.state(BuiltInEvent.ENTRY_SIG)
+        Returns:
+            state_machine.Status: the status
+        """
+        status = state_machine.Status.IGNORED_STATUS
+
+        if event is RobotEvent.ENTRY_SIG:
+            rclpy.logging._root_logger.info("Robot in initial state")
+            status = state_machine.Status.TRAN_STATUS
+
+        else:
+            if self.robot.is_ready:
+                rclpy.logging._root_logger.info("All sensor datas are ready")
+                self.transition_to(self.configuration_state)
+                status = state_machine.Status.TRAN_STATUS
+
+        return status
     
-    def initial_state(self, event):
+    def configuration_state(self, event: RobotEvent) -> state_machine.Status:
+        """
+        The configuration state of the robot FSM.
         
-        if event is BuiltInEvent.ENTRY_SIG:
-            return NotImplemented
-        elif event is BuiltInEvent.EXIT_SIG:
-            return NotImplemented
-        elif event is BuiltInEvent.INIT_SIG:
+        Args:
+            event (RobotEvent): the event
+                
+        Returns:
+            state_machine.Status: the status
+        """
+        status = state_machine.Status.IGNORED_STATUS
 
-"""
+        if event is RobotEvent.ENTRY_SIG:
+            # Move to default joint in damping mode
+            self.controller.node_handler.joint_cmd_pub.publish(self.controller._joint_cmd_damping_msg)
 
-class FSM(object):
-    def __init__(self):
-        self.state = self.initial_state
+            # Set the timer_counter to 0
+            self.controller.node_handler.timer_counter = 0
+            rclpy.logging._root_logger.info("Robot in configuration state")
+            status = state_machine.Status.HANDLED_STATUS
 
-    def dispatch(self, event: BuiltInEvent) -> None:
-        prev_state = self.state
-        status = self.state(event)
+        elif event is RobotEvent.TIME_OUT_2S:
+            self.transition_to(self.running_state)
+            status = state_machine.Status.TRAN_STATUS
 
-        if status is Status.TRAN_STATUS:
-            prev_state(BuiltInEvent.EXIT_SIG)
-            self.state(BuiltInEvent.ENTRY_SIG)
 
-    def transition_to(self, state) -> None:
-        self.state = state
+        return status
+    
+    def running_state(self, event: RobotEvent) -> state_machine.Status:
+        """
+        The running state of the robot FSM.
+        
+        Args:
+            event (RobotEvent): the event
+                
+        Returns:
+            state_machine.Status: the status
+        """
+        status = state_machine.Status.IGNORED_STATUS
 
-    def initial_state(self, event) -> Status:
-        return NotImplemented
+        # 50Hz
+        if event is RobotEvent.TIMER_EVENT:
+            # Compute the joint commands
+            joint_cmds = self.controller.compute().tolist()
+            
+            # Publish the joint commands, velocity field is used to set kp and kd
+            joint_cmd_msg = JointState()
+            for joint_cmd in joint_cmds:
+                joint_cmd_msg.position.append(joint_cmd)
+            joint_cmd_msg.velocity = self.robot_controller.config.kps + self.robot_controller.config.kds
+            self.node_handler.joint_cmd_pub.publish(joint_cmd_msg)
+
+            status = state_machine.Status.HANDLED_STATUS
+        
+        # Default
+        else:
+            # check safety
+            status = state_machine.Status.HANDLED_STATUS
+
+
+        return status
+
+    def error_state(self, event: RobotEvent) -> state_machine.Status:
+        """
+        The error state of the robot FSM.
+        
+        Args:
+            event (RobotEvent): the event
+                
+        Returns:
+            state_machine.Status: the status
+        """
+        status = state_machine.Status.IGNORED_STATUS
+
+        if event is RobotEvent.ENTRY_SIG:
+            # Stop the all the motors
+            self.controller.stop()
+            
+            # Reset the robot controller
+            self.controller.current_controller.reset()
+            
+            # Log error
+            rclpy.logging._root_logger.error("Robot in error state")
+            status = state_machine.Status.HANDLED_STATUS
+
+        return status
